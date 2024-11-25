b009d127d231d6e67f2743052d0706e7f1d15b68
Ravi-dhanani <ravidhanani@gmail.com>
2024-11-25 22:22:02 +0530

    API url change

--- src/components/services/Apiservices.tsx
+++ src/components/services/Apiservices.tsx
@@ -4,11 +4,11 @@ import HttpService from "./HttpService";
 const Base_URl = process.env.NEXT_PUBLIC_API_URL;
 class ApiServices {
   static register(data: any) {
-    return HttpService.post(`${Base_URl}/auth/login`, data);
+    return HttpService.post(`${Base_URl}/api/auth/login`, data);
   }
 
   static login(data: any) {
-    let res = HttpService.post(`${Base_URl}/auth/login`, data);
+    let res = HttpService.post(`${Base_URl}/api/auth/login`, data);
 
     return res;
   }
